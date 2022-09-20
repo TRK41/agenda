3d13f19491a5fd239e435039cbb0b7d217177354
TRK41 <Troyk@bell.net>
2022-09-19 23:43:39 -0400

    finalizing code

--- Develop/script.js
+++ Develop/script.js
@@ -11,12 +11,11 @@ var t5 = document.getElementById("five");
 const saveBtnArr = document.querySelectorAll('button');
 var arr = $(".8am")
 
-
 for (let saveBtn of saveBtnArr) {
 
      saveBtn.addEventListener('click', function (event) {
           event.preventDefault();
-          localStorage.setItem("8am", JSON.stringify(t8.value));
+          localStorage.setItem("8am", JSON.stringify(t8.value));// adding the key and value for local storage
           localStorage.setItem("9am", JSON.stringify(t9.value));
           localStorage.setItem("10am", JSON.stringify(t10.value));
           localStorage.setItem("11am", JSON.stringify(t11.value));
@@ -27,49 +26,32 @@ for (let saveBtn of saveBtnArr) {
           localStorage.setItem("4pm", JSON.stringify(t4.value));
           localStorage.setItem("5pm", JSON.stringify(t5.value));
 
-     //     //localStorage.setItem('8am',timeBlock.value);
+
+     
      
      function saveTime() {
           let notes = localStorage.getItem("notes")
-          var storedNotes = JSON.parse(localStorage.getItem("t8"));
+          var storedNotes = JSON.parse(localStorage.getItem("8am",t8.value));
           
           if (storedNotes) {
               notes = storedNotes;
               return;
           }
+          localStorage.setItem(x,storedNotes);
      }
-          t8.innerHTML=localStorage.notes;
+          
           console.log();
-          saveTime();
-     
+          saveTime()
 });
 }
-// saveBtn.addEventListener('click', function (event) {
-//      event.preventDefault();
-//      localStorage.setItem("9am", JSON.stringify(t9.value));
-//      t9.value.innerHTML = this.value 
-// //     //localStorage.setItem('8am',timeBlock.value);
-
-// function saveTime() {
-//      let notes = localStorage.getItem("notes")
-//      var storedNotes = JSON.parse(localStorage.getItem("t9"));
-
-//      if (storedNotes) {
-//          notes = storedNotes;
-//          return;
-//      }
-// }
-//      t9.innerHTML=localStorage.notes;
-//      console.log();
-//      saveTime();
-// });
-
 
 //current day time (real time)
 setInterval(function time() {
      var todayDate = moment().format('MMMM Do YYYY h:mm:ss a');
      $("#currentDay").text(todayDate);
 }, 1000);
+
+
 
 //8am
 //  const eight = '08:00:00'
